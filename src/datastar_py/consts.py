933dce0bf2a81c9f276ca7f71946a191614a626d
Delaney <delaneygillilan@gmail.com>
2025-01-12 18:16:25 -0800

    Better error on invalid expressions and signals (#466)
    * Better error messages for nested signals
    Fixes #452
    
    * Add explicit check for invalid signal
    
    * move to generic errors
    
    * working towards better error pages
    
    * Tweak errors
    
    * more complete example
    
    * bad templ
    
    * Start porting over errors
    
    * Better error messages for nested signals
    Fixes #452
    
    * fix nil pointers
    
    * Migrate remaining errors
    
    * Fix init errors
    
    * Tweaks
    
    * Add suggested signal names
    
    * Improve styling and close input elements
    
    * Cleanup
    
    * syntax highlight
    
    * Fixes
    
    * More fixes
    
    * Tweaks
    
    * Update library size approximation
    
    * fix lines and escaping
    
    * add if
    
    * Finish default error messages
    
    * Finish PR
    
    ---------
    
    Co-authored-by: Ben Croker <ben@putyourlightson.com>

--- src/datastar_py/consts.py
+++ src/datastar_py/consts.py
@@ -52,8 +52,8 @@ class EventType(StrEnum):
 
 DATASTAR_KEY = "datastar"
 VERSION                   = "1.0.0-beta.1"
-VERSION_CLIENT_BYTE_SIZE     = 35295
-VERSION_CLIENT_BYTE_SIZE_GZIP = 12940
+VERSION_CLIENT_BYTE_SIZE     = 36235
+VERSION_CLIENT_BYTE_SIZE_GZIP = 13244
 
 #region Default durations
 
