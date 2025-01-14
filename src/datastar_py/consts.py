e4a2837e59a62a7be2ff15a5c581553a99a0b990
Delaney <delaneygillilan@gmail.com>
2025-01-13 17:19:58 -0800

    Rename nested signals in library (#480)
    Fixes #478

--- src/datastar_py/consts.py
+++ src/datastar_py/consts.py
@@ -53,7 +53,7 @@ class EventType(StrEnum):
 DATASTAR_KEY = "datastar"
 VERSION                   = "1.0.0-beta.1"
 VERSION_CLIENT_BYTE_SIZE     = 36235
-VERSION_CLIENT_BYTE_SIZE_GZIP = 13244
+VERSION_CLIENT_BYTE_SIZE_GZIP = 13242
 
 #region Default durations
 
